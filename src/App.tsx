a3ffb8100fe1d31587f1820d045b7396b7603802
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-01 15:36:16 +0200

    home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
-import './App.css'
-import Header from './components/Header'
-import Home from './components/Home'
-
-function App() {
-
+function App(): JSX.Element {
   return (
     <>
-   <Header />
-   <Home />
+      <Header />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
